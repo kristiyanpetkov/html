9ab08dc934776640ca83a2dc7b122eecf3f91e4b
slavidichkov <dichkof@gmail.com>
2016-03-02 14:59:35 +0200

    fixed usage of '.' in one number more than once

--- src/com/clouway/task6/calculator.js
+++ src/com/clouway/task6/calculator.js
@@ -2,30 +2,50 @@
  * Created by com on 16-2-25.
  */
 
- function clean() {
-    document.getElementById("input").innerHTML = "";
- }
+function clean() {
+    setValue("input", "");
+}
 
- function deleteLastChar() {
-    var value = document.getElementById("input").innerHTML;
-    document.getElementById("input").innerHTML = value.slice(0, -1);
- }
+function deleteLastChar() {
+    var value = getValue("input");
+    setValue("input", value.slice(0, -1));
+}
 
- function add(val) {
-    var currentValue = document.getElementById("input").innerHTML;
-    var s = ['.', '*', '/', '+', '-', '%'];
-    console.log(s.indexOf(currentValue.slice(-1)));
-    if (s.indexOf(currentValue.slice(-1)) < 0) {
-        document.getElementById("input").innerHTML = (currentValue + val);
+function add(val) {
+    var currentValue = getValue("input");
+    var signsArray = ['.', '*', '/', '+', '-', '%'];
+    if ((currentValue && signsArray.indexOf(currentValue.slice(-1)) == -1) || signsArray.indexOf(val) == -1) {
+        if (lastCharInValueFromArray(currentValue,signsArray) != '.' && val == '.' ){
+            setValue("input", currentValue + val);
+        }else if (val != '.'){
+            setValue("input", currentValue + val);
+        }
     }
+}
 
- }
+function lastCharInValueFromArray(value, signsArray){
+    var char='';
+    for (var i=0; i < value.length; i++ ){
+        if (signsArray.indexOf(value.charAt(i)) != -1){
+            char=value.charAt(i);
+        }
+    }
+    return char;
+}
 
- function compute() {
+function compute() {
     try {
-        document.getElementById("input").innerHTML = (eval(document.getElementById("input").innerHTML));
+        setValue("input", eval(getValue("input")));
     }
     catch (e) {
         c('Error')
     }
- }
+}
+
+function getValue(elementId) {
+    return document.getElementById(elementId).innerHTML;
+}
+
+function setValue(elementId, value) {
+    document.getElementById(elementId).innerHTML = value;
+}
