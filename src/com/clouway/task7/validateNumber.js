43ddbe182d2dec5a670bd25b3293152aad091e62
slavidichkov <dichkof@gmail.com>
2016-03-02 09:20:26 +0200

    wrote function setVisibility

--- src/com/clouway/task7/validateNumber.js
+++ src/com/clouway/task7/validateNumber.js
@@ -5,10 +5,13 @@ function isValidNumber(inputID) {
     var validator = new RegExp("^(0)$|^([1-9]){1,10}$|^([1-9]){1,10}\.([0-9]){1,5}$|^(0)\.([0-9]){1,5}.$");
     var inputValue = document.getElementById("number").value;
     if (!validator.test(inputValue)) {
-        document.getElementById("incorrect").setAttribute("class", "invalid");
-        document.getElementById("incorrect").innerHTML = "number is incorrect";
+        setVisibility("wrongNumber","visible");
     } else {
-        document.getElementById("incorrect").setAttribute("class", "valid");
-        document.getElementById("incorrect").innerHTML = "number is correct";
+        setVisibility("wrongNumber","hidden");
     }
+}
+function setVisibility(elementName,visibility){
+    var element = document.getElementById(elementName);
+    element.innerHTML = " incorrect";
+    element.style.visibility = visibility;
 }
