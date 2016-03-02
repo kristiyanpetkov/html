23dd5f613376ad1b0086ed3a30283420e88ef604
slavidichkov <dichkof@gmail.com>
2016-03-02 15:53:04 +0200

    add css file and add dependencies to the function

--- src/com/clouway/task4/emptyField.js
+++ src/com/clouway/task4/emptyField.js
@@ -1,12 +1,12 @@
 /**
  * Created by clouway on 16-2-29.
  */
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
+function validateForm(formName, fNameId, displayId) {
+    var x = document.forms[formName][fNameId].value;
     if (x == null || x == "") {
-        document.getElementById("demo").innerHTML = " Name must be filled out";
+        document.getElementById(displayId).innerHTML = " Name must be filled out";
         return false;
     }else {
-        document.getElementById("demo").innerHTML = null;
+        document.getElementById(displayId).innerHTML = null;
     }
 }
