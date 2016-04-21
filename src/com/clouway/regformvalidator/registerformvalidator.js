cc3352f13c81b03d7ec344f2895ef1805c5ebe87
kristiyanpetkov <kristiqn.l.petkov@gmail.com>
2016-04-21 09:58:24 +0300

    Added check if there is an empty filed the registration will fail

--- src/com/clouway/regformvalidator/registerformvalidator.js
+++ src/com/clouway/regformvalidator/registerformvalidator.js
@@ -1,4 +1,3 @@
-
 function validate(fieldName, regexp, errorMsg, errorField) {
   console.log(fieldName);
   var valuee = document.forms["form"][fieldName].value;
@@ -11,7 +10,21 @@ function validate(fieldName, regexp, errorMsg, errorField) {
 
 function Submit() {
   var successfullMessage;
-  if (document.getElementById("errorBox").innerHTML == "") {
+  var age = document.form.age.value;
+  var egn = document.form.egn.value;
+  var address = document.form.address.value;
+  var lastname = document.form.lastname.value;
+  var firstname = document.form.firstname.value;
+  var passowrd = document.form.password.value;
+  var cpassword = document.form.cpassword.value;
+  if (document.getElementById("errorBox").innerHTML == ""
+          && Object.keys(cpassword).length != 0
+          && Object.keys(firstname).length != 0
+          && Object.keys(lastname).length != 0
+          && Object.keys(age).length != 0
+          && Object.keys(address).length != 0
+          && Object.keys(egn).length != 0
+          && Object.keys(passowrd).length != 0) {
     successfullMessage = "Registration successful!";
     document.getElementById("regSuccessBox").innerHTML = successfullMessage.fontcolor("green");
   } else {
